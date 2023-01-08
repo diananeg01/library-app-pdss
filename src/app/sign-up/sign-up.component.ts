be3019403e2f71d55417b4c9506e95604aa37cc3
diananeg01 <diananegurita07@gmail.com>
2023-01-08 22:50:59 +0200

    add "Sign in via Google" provider to user

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {UserEndpointService} from "../endpoints/user-endpoint.service";
-import {  createUserWithEmailAndPassword } from "firebase/auth";
-import {Auth} from "@angular/fire/auth";
+import { createUserWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+import {Auth, signInWithPopup} from "@angular/fire/auth";
 import {MessageService} from "primeng/api";
 import {Database, ref, set} from "@angular/fire/database";
 
@@ -19,15 +19,35 @@ export class SignUpComponent {
 
   constructor(private router: Router,
               private auth: Auth,
+              private provider: GoogleAuthProvider,
               private database: Database,
               private userEndpointService: UserEndpointService,
               private messageService: MessageService) {
+    this.provider = new GoogleAuthProvider();
   }
 
-  // loginWithGoogle(): void {
-  //   this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
-  //     .then(() => this.router.navigate(['sign-up/personal-information']));
-  // }
+  loginWithGoogle(): void {
+    signInWithPopup(this.auth, this.provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential!.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        this.router.navigate(['sign-up/personal-information']);
+      }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      this.messageService.add({severity:'error', summary: 'Error', detail: errorMessage, life: 3000, key: 'custom-error'});
+      // The email of the user's account used.
+      const email = error.customData.email;
+      this.messageService.add({severity:'error', summary: 'Error', detail: "Email already in use!", life: 3000, key: 'used-email'});
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  }
 
   ngOnInit(): void {
     this.username = '';
